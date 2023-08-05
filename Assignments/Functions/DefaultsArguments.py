dcc3d2b35953ca7f8d9a78c44ea7ab46367ec914
THARIQRAHMAN <thariqrehman@gmail.com>
2023-08-05 13:00:03 +0530

    COMMIT PROGRAM

--- Assignments/Functions/DefaultsArguments.py
+++ Assignments/Functions/DefaultsArguments.py
@@ -1,1 +1,7 @@
-print("kuthir")
+#program for Default Arguments
+def add(x,y=600):
+    "Calculate percentage for 12th mark"
+    return str(x/600*100) + "%"
+n= int(input("Enter the Mark that you Gained in 12th Standard: "))
+print(add(n))
+print(add.__doc__)
